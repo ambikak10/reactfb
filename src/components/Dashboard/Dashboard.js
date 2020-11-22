b11cd4219747965d740f3570607a13b78e642a66
Ambika K <ambikak10@gmail.com>
2020-11-21 21:28:25 -0800

    initial commit
    created react app

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+import './dashboard.css'
+
+class Dashboard extends Component {
+  render() {
+    return (
+      <div className='container margin'>
+        <div className='row'>
+          <div className='col-xl-4 col-lg-4 col-md-4'>Hi</div>
+          <div className='col-xl-8 col-lg-8 col-md-8'>Hii</div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Dashboard
---
+++ src/components/Login/Login.js
@@ -0,0 +1,64 @@
+import React, { Component } from "react";
+import FacebookLogin from "react-facebook-login";
+import './login.css'
+
+class Login extends Component {
+  constructor() {
+    super();
+    this.state = {
+      isLoggedIn: false,
+      name: "",
+      email: "",
+      address: "",
+      phone: "",
+      userID: "",
+      access: "",
+    };
+  }
+  componentClicked = (e) => {
+    console.log("clicked");
+    console.log(this.state.userID);
+    this.props.history.push("/dashboard", {
+      id: this.state.userID,
+      access: this.state.access,
+    });
+  };
+  responseFacebook = (response) => {
+    console.log(response)
+    this.setState({
+      name: response.name,
+      email: response.email,
+      address: response.address,
+      phone: response.phone,
+      userID: response.id,
+      access: response.accessToken,
+      isLoggedIn: true
+    });
+  };
+  render() {
+    let fbContent;
+    // if (this.state.isLoggedIn) {
+    //   this.props.history.push("/dashboard")
+    // } else {
+      fbContent = (
+        <FacebookLogin
+          appId='379462766637679'
+          autoLoad={true}
+          fields='name,email,picture'
+          callback={this.responseFacebook}
+          onClick={this.componentClicked}
+        />
+      );
+  //  }
+    return (
+      <div className='App App-logo'>
+        <header className='App-header'>
+          To get started, authenticate with Facebook
+          <br></br>
+          {fbContent}
+        </header>
+      </div>
+    );
+  }
+}
+export default Login;

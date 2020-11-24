70bbb1e1b830d15f90da16063ecf14c1b45dbbe0
Ambika K <ambikak10@gmail.com>
2020-11-23 20:52:00 -0800

    removed browser router

--- src/index.js
+++ src/index.js
@@ -3,13 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import BrowserRouter from "react-router-dom/BrowserRouter";
 
 ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>,
-  document.getElementById("root")
+  </React.StrictMode>,
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
